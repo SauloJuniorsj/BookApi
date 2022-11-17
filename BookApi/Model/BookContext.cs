826c6b864fcf532545ef2996252f865edb2f9b77
SauloJuniorsj <juniorroseiro@hotmail.com>
2022-11-17 00:15:20 -0300

    Adding the controller and context

---
+++ BookApi/Model/BookContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookApi.Model
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+            Database.EnsureCreated(); 
+        }
+        public DbSet<Book> Books { get; set; } //cada entitade precisa ser mapeada aqui
+    }
+}
